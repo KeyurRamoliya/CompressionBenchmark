556646764e006381e3de951321369e2778c9394e
Keyur Ramoliya <keyurramoliya19@gmail.com>
2023-09-10 10:51:17 +0530

    Initial Commit

---
+++ CompressionBenchmark/DeflateBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.IO.Compression;
+
+namespace CompressionBenchmark
+{
+    // Enable memory allocation diagnostics
+    [MemoryDiagnoser]
+    public class DeflateBenchmark
+    {
+        // Specify the compression level parameter for Deflate compression
+        [Params(CompressionLevel.Optimal, CompressionLevel.Fastest, CompressionLevel.NoCompression, CompressionLevel.SmallestSize)]
+        public CompressionLevel CompressionLevel { get; set; }
+
+        // Define some sample data to compress
+        private byte[] data = System.Text.Encoding.UTF8.GetBytes("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lorem vitae nisi consequat varius. Morbi id leo quis augue aliquet lacinia. Fusce vitae semper nisl. Mauris quis leo sit amet lacus tincidunt sagittis. Vivamus euismod, nisi quis condimentum ultrices, nunc eros malesuada leo, at tincidunt lectus urna sed leo. Quisque nec sapien ut nisl vulputate ullamcorper. Sed id semper diam. Donec eget justo et justo aliquam sollicitudin. Suspendisse potenti. Curabitur sit amet lorem ac erat sagittis consequat.");
+
+        // Define a stream for Deflate compression
+        private MemoryStream compressedStream;
+
+        // Initialize the Deflate compression stream
+        [GlobalSetup]
+        public void Setup()
+        {
+            compressedStream = new MemoryStream();
+        }
+
+        // Measure the performance of Deflate compression on the sample data
+        [Benchmark]
+        public void Compress()
+        {
+            compressedStream.Position = 0;
+            var deflateStream = new DeflateStream(compressedStream, CompressionLevel, true);
+            deflateStream.Write(data, 0, data.Length);
+            deflateStream.Flush();
+        }
+
+        // Dispose the Deflate compression stream
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            compressedStream.Dispose();
+        }
+    }
+}
+
---
+++ CompressionBenchmark/Program.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace CompressionBenchmark;
+
+// Specify the number of warmup and benchmark iterations
+[SimpleJob(warmupCount: 3, iterationCount: 5)]
+
+// Export the results to a markdown table
+[MarkdownExporterAttribute.GitHub]
+class Program
+{
+    static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<BrotliBenchmark>();
+        summary = BenchmarkRunner.Run<GzipBenchmark>();
+        summary = BenchmarkRunner.Run<DeflateBenchmark>();
+
+        Console.WriteLine("Done");
+        Console.ReadLine();
+    }
+}
